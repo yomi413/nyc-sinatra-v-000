182a098e232daa3eae2d282e87c0350580a61ccf
Yomaira Escano <yjescano@gmail.com>
2018-02-06 15:12:10 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark_year_completed])
+
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
